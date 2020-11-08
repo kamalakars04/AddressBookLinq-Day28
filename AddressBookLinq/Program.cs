43b70c4c363700a0fc4296c72c6af434f118b164
kamalakars04 <kamalakars04@gmail.com>
2020-11-08 23:48:18 +0530

    [Kamalakar].[Refactor].Added UC5_DeleteContact

--- AddressBookLinq/Program.cs
+++ AddressBookLinq/Program.cs
@@ -16,7 +16,13 @@ namespace AddressBookLinq
             AddressBookTable addressBookTable = new AddressBookTable();
 
             // UC 4 Edit Existing Contact
+            Console.WriteLine("After Editing Existing Contact");
             addressBookTable.EditExistingContact("Kamalakar", "Rao", "PhoneNumber", "8919810751");
+            addressBookTable.GetAllContacts();
+
+            // UC 5 Delete Existing Contact
+            Console.WriteLine("After Deleting Contact");
+            addressBookTable.DeleteContact("Apoorva", "Singh");
             addressBookTable.GetAllContacts();
         }
     }
