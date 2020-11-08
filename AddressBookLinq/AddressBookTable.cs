9b92ef657413e85f9aa31ba55e9415f75f632c37
kamalakars04 <kamalakars04@gmail.com>
2020-11-08 22:51:05 +0530

    [Kamalakar].[Refactor].Added UC2_AddAttributes

--- AddressBookLinq/AddressBookTable.cs
+++ AddressBookLinq/AddressBookTable.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="fileName.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Your name"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace AddressBookLinq
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text;
+
     public class AddressBookTable
     {
         // UC 1 Create a new DataTable
         DataTable table = new DataTable("AddressBook");
+
+        /// <summary>
+        /// UC 2 Initializes a new instance of the <see cref="AddressBookTable"/> class.
+        /// </summary>
+        public AddressBookTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
